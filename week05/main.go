090d4fe816a8dc32d758af840022a570ca438b91
odyssey02 <odysseydev02@gmail.com>
2023-09-25 17:39:44 +0900

    week05 v0.9

--- week05/main.go
+++ week05/main.go
@@ -5,19 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n') //option 2
+	inputScoreString, err := reader.ReadString('\n') //option 2
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(inputScore)
+	inputScoreString = strings.TrimSpace(inputScoreString)      //remove space
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32) //string to 32bit float
+	var grade string
 	if inputScore >= 90 { //mismatched types string and untyped int
-		grade := "A grade!"
+		grade = "A grade!"
 	} else {
-		grade := "under A grade..."
+		grade = "under A grade..."
 	}
+	fmt.Println("Your got", grade)
 }
