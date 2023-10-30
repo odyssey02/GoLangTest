7d765822d5eb102b6e7bd77a5e8d55eeb5b77982
odyssey02 <odysseydev02@gmail.com>
2023-10-30 17:08:35 +0900

    v1.10

---
+++ week10/src/mymath/mymath.go
@@ -0,0 +1,16 @@
+package mymath
+
+func MyPower(base int, exponent int) int {
+	result := 1
+	for i := 1; i <= exponent; i++ {
+		result *= base
+	}
+	return result
+}
+
+func MyAbs(number int) int {
+	if number < 0 {
+		return -number
+	}
+	return number
+}
