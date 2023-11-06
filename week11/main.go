9d9674191e43f293c57e12f3f77a6d65caaa92d3
odyssey02 <odysseydev02@gmail.com>
2023-11-06 17:04:51 +0900

    v1.20

---
+++ week11/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var primes [3]int
+	// primes[0] = 2
+	// primes[1] = 3
+	// primes[2] = 5
+	// var primes [3]int = [3]int{2, 3, 5}
+	primes := [3]int{2, 3, 5}
+	fmt.Println(primes, primes[1])
+}
