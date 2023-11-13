9489f7b13cce455a3982f32755e28aca87742441
odyssey02 <odysseydev02@gmail.com>
2023-11-13 17:51:40 +0900

    v1.36

--- week12/main.go
+++ week12/main.go
@@ -12,6 +12,9 @@ func main() {
 	// c := append(a, "y")
 	c := append(a, "y", "x")
 
+	c[0] = "q"
+
 	fmt.Println(a, len(a), cap(a))
 	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
 }
