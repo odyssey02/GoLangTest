81a2af25e244140afa4c14012031f9eba399de2a
odyssey02 <odysseydev02@gmail.com>
2023-11-13 16:59:29 +0900

    v1.31

--- week12/main.go
+++ week12/main.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
-	s = make([]int, 5)
+	// var s []int
+	// s = make([]int, 5)
+
+	// s := make([]int, 5) //단축연산자 사용 초기화
+
+	s := []int{0, 0, 0, 0, 0} //단축연산자, 슬라이스리터럴 사용 초기화
+
+	s[4] = 100
+	s[0] = 7
+	s[1] = 91
 
 	for _, value := range s {
 		fmt.Println(value)
